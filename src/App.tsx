f0bda0d51263de0287b18ac470134bf8ac236801
Ahmet Deveci <ahmet.deveci@commencis.com>
2023-04-05 01:32:12 +0300

    Add service calls
    - Weather call for after position detected
    - IP call for when location permission cannot get
    - Weather call after IP position detected

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {IPService} from "./services/IPService";
+import {WeatherService} from "./services/WeatherService";
+
+type Position = {
+    latitude: number;
+    longitude: number;
+};
 
 const App = (): JSX.Element => {
-    const [position, setPosition] = useState<GeolocationPosition | null>(null);
+    const [position, setPosition] = useState<Position | null>(null);
     const [visible, setVisible] = useState<string>(document.visibilityState);
+    const [weather, setWeather] = useState<any>(null);
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
-            setPosition(position);
-        }, (error: GeolocationPositionError) => {
-            console.log(error);
+            const {latitude, longitude} = position.coords;
+            setPosition({latitude, longitude});
+        }, async (error: GeolocationPositionError) => {
+            getIPData();
         });
         document.addEventListener("visibilitychange", () => {
             setVisible(document.visibilityState);
         });
     }, []);
 
+    const getWeatherData = async () => {
+
+        WeatherService.getWeather(`${position?.latitude},${position?.longitude}`, 'current').then((result: any) => {
+            // console.log(result);
+            setWeather(result.data);
+        }).catch((error: any) => {
+            console.log(error);
+        });
+    };
+
+    const getIPData = async () => {
+        const ipData = await IPService.getIP();
+        const {lat: latitude, lon: longitude} = ipData.data;
+        setPosition({latitude, longitude});
+    };
+
+    useEffect(() => {
+        if (position) {
+            getWeatherData();
+        }
+    }, [position]);
+
+    // TODO: Implement layout
     return (
         <div className="App">
             <header className="App-header">
